5768d3cf606ac2c780f103ae2e31764386dc2c5c
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-18 12:15:34 +0530

    refactoring code

--- script.js
+++ script.js
@@ -6,36 +6,37 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-// Dark Mode Styles
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].className = 'fas fa-moon';
-    image1.src = 'img/undraw_proud_coder_dark.svg';
-    image2.src = 'img/undraw_feeling_proud_dark.svg';
-    image3.src = 'img/undraw_conceptual_idea_dark.svg';
-}
+// Switch Mode
+function switchMode(mode, values){
+    // Setting color sheme for different modes
+    document.documentElement.setAttribute('data-theme', mode);
 
-// Light Mode Styles
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].className = 'fas fa-sun';
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
+    // Changing BgColor, Content
+    nav.style.backgroundColor = values.navBgColor;
+    textBox.style.backgroundColor = values.textBoxBgColor;
+    toggleIcon.children[0].textContent = values.toggleTextContent;
+    toggleIcon.children[1].className = `fas fa-${values.toogleIcon}`;
+    image1.src = `img/undraw_proud_coder_${mode}.svg`;
+    image2.src = `img/undraw_feeling_proud_${mode}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        switchMode('dark', {
+            navBgColor: 'rgb(0 0 0 / 50%)',
+            textBoxBgColor: 'rgb(255 255 255 / 50%)',
+            toggleTextContent: 'Dark Mode',
+            toogleIcon: 'moon',
+        })
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        switchMode('light', {
+            navBgColor: 'rgb(255 255 255 / 50%)',
+            textBoxBgColor: 'rgb(0 0 0 / 50%)',
+            toggleTextContent: 'Light Mode',
+            toogleIcon: 'sun',
+        })
     }
 }
 
